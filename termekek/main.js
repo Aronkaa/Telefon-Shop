50ab3d625492a3f77094b63e2916c7e62e47bb35
Aronkaa <aaron.majoros@gmail.com>
2022-03-13 20:38:17 +0100

    Update V1

---
+++ termekek/main.js
@@ -0,0 +1,10 @@
+function photoview(img) {
+  const main = document.querySelector("main");
+  const template = `<div class="outter">
+      <div class="photo">
+        <img src="/telefonok/${img}" alt="" />
+        <button onclick="close()">X</button>
+      </div>
+    </div>`;
+  main.innerText = main.innerText + template;
+}
